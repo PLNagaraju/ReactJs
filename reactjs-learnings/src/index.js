214cb0d5c06e43932ab5a8c2229cb8740f96914d
PLNAGARAJU\Nagaraju <plnr206@gmail.com>
2021-07-14 15:43:04 +0530

    Usage of ES7 snippets extension - boiler plate code

--- reactjs-learnings/src/index.js
+++ reactjs-learnings/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from 'react'
+import ReactDom from 'react-dom'
 
 function Greeting() {
   return <h2>Hello World !! Welcome to React Learning</h2>
 }
 
-ReactDom.render(<Greeting />, document.getElementById('root'));
+ReactDom.render(<Greeting />, document.getElementById('root'))
---
+++ reactjs-learnings/src/testsnippet.js
@@ -0,0 +1,10 @@
+// created with rafce -> enter
+// Req : ES7 snippets extension
+
+import React from 'react'
+
+const testsnippet = () => {
+  return <div></div>
+}
+
+export default testsnippet
